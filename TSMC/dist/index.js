27b17e360d331d1dd54782596560d2e5b242ba52
Ventsislav Peychev <del.busque@gmail.com>
2024-06-17 14:14:25 +0300

    Extending interfaces and types

--- TSMC/dist/index.js
+++ TSMC/dist/index.js
@@ -1,9 +1,24 @@
 "use strict";
+const ford = {
+    id: 1,
+    year: 1988,
+    manufacturer: 'Ford',
+    model: 'Focus',
+    report() {
+        return `${this.id} ${this.model}`;
+    }
+};
 let greet = (person) => {
-    console.log(`${person.name} is ${person.age} years old...`);
+    console.log(`${person.name} is ${person.age} years old..`);
 };
 const person = {
     name: 'Ivan',
-    age: 33
+    age: 33,
+    email: 'hi@hi.com',
+    greet() {
+        return `${this.name} is ${person.age} years old... @ ${this.email}`;
+    }
 };
+console.log(person.greet());
 greet(person);
+console.log(ford.report());
--- TSMC/src/index.ts
+++ TSMC/src/index.ts
@@ -1,16 +1,49 @@
-interface Person{
+interface Person {
     name: string,
     age: number
 }
 
-let greet = (person: Person): void =>{
-    console.log(`${person.name} is ${person.age} years old...`);
-    
+interface User extends Person {
+    email: string,
+    greet: () => string
+}
+
+type Vehicle = {
+    id: string | number,
+    year: number
+}
+
+type Car = Vehicle & {
+    manufacturer: string,
+    model: string
+    report(): string
+}
+
+const ford: Car = {
+    id: 1,
+    year: 1988,
+    manufacturer: 'Ford',
+    model: 'Focus',
+    report(): string {
+        return `${this.id} ${this.model}`
+    }
+}
+
+let greet = (person: Person): void => {
+    console.log(`${person.name} is ${person.age} years old..`);
 }
 
 const person = {
     name: 'Ivan',
-    age: 33
+    age: 33,
+    email: 'hi@hi.com',
+    greet(): string {
+        return `${this.name} is ${person.age} years old... @ ${this.email}`
+    }
 }
 
-greet(person);
+console.log(person.greet());
+
+greet(person)
+
+console.log(ford.report());
